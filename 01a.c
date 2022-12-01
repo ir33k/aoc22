9218eb931524264a910e270d7267fb23d35f8d52
irek <irek@gabr.pl>
2022-12-01 06:42:28 +0100

    01a

---
+++ 01a.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZ	2056		/* Max number of Elfs */
+
+int
+main(int argc, char **argv)
+{
+	size_t i, arri = 0;
+	unsigned long int arr[SIZ] = {0}, max = 0;
+	char buf[BUFSIZ];
+	FILE *fp = stdin;
+
+	if (argc > 1 && (fp = fopen(argv[1], "rb")) == NULL) {
+		perror("fopen");
+		return 1;
+	}
+	while (fgets(buf, BUFSIZ, fp)) {
+		if (buf[0] != '\n') {
+			arr[arri] += atoi(buf);
+			continue;
+		}
+		arri++;
+		if (arri > SIZ) {
+			fprintf(stderr, "SIZ too small");
+			return 1;
+		}
+	}
+	for (i = 0; i <= arri; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	printf("%lu\n", max);
+	if (fp != stdin && fclose(fp) == EOF) {
+		perror("fclose");
+		return 1;
+	}
+	return 0;
+}
