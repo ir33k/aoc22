ff77277c9473647e4692dd19ed5be7d233695346
Ireneusz Gabryś <ireneusz.gabrys@optioincentives.no>
2022-12-02 10:39:02 +0100

    Use a bit of math to make part 2 of day 2 more compact

--- 02b.c
+++ 02b.c
@@ -3,71 +3,33 @@
 
 #define BSIZ    8		/* Buffer size */
 
-enum shape { ROCK, PAPER, SCISSORS };
-enum score_shape {
-	SCORE_ROCK     = 1,
-	SCORE_PAPER    = 2,
-	SCORE_SCISSORS = 3
-};
-enum score_res {
-	SCORE_LOST     = 0,
-	SCORE_DRAW     = 3,
-	SCORE_WON      = 6
-};
+enum { ROCK = 0, PAPER, SCISSORS, LOST, DRAW, WON };
+int score[] = { 1, 2, 3, 0, 3, 6 }; /* Match enum */
 
-enum shape
-parse(char c)
+/* Parse C char (A,B,C) to shape of ROCK, PAPER or SCISSORS. */
+int
+parse_shape(char c)
 {
-	switch (c) {
-	case 'A': return ROCK;
-	case 'B': return PAPER;
-	case 'C': return SCISSORS;
-	}
-	errx(1, "Invalid shape '%c'", c);
+	return c - 'A';
 }
 
-enum score_shape
-score_shape(enum shape s)
+/* Parse C char (X,Y,Z) to outcome of LOSE, DRAW or WON. */
+int
+parse_out(char c)
 {
-	switch (s) {
-	case ROCK:     return SCORE_ROCK;
-	case PAPER:    return SCORE_PAPER;
-	case SCISSORS: return SCORE_SCISSORS;
-	}
-	errx(1, "Invalid shape '%c'", s);
+	return c - 'X' + LOST;	/* Add LOST offset from enum */
 }
 
-enum score_res
-score_res(char c)
+/* With given outcome OUT and first player shape P1 return proper
+ * shape for second player that match OUT. */
+int
+out2shape(int out, int p1)
 {
-	switch (c) {
-	case 'X': return SCORE_LOST;
-	case 'Y': return SCORE_DRAW;
-	case 'Z': return SCORE_WON;
+	switch (out) {
+	case LOST: return (p1 + 2) % 3;
+	case WON:  return (p1 + 1) % 3;
 	}
-	errx(1, "Invalid outcome '%c'", c);
-}
-
-enum score_shape
-score_res_shape(enum score_res res, enum shape p1)
-{
-	switch (res) {
-	case SCORE_LOST:
-		switch (p1) {
-		case ROCK:     return SCORE_SCISSORS;
-		case PAPER:    return SCORE_ROCK;
-		case SCISSORS: return SCORE_PAPER;
-		}
-	case SCORE_DRAW:
-		return score_shape(p1);
-	case SCORE_WON:
-		switch (p1) {
-		case ROCK:     return SCORE_PAPER;
-		case PAPER:    return SCORE_SCISSORS;
-		case SCISSORS: return SCORE_ROCK;
-		}
-	}
-	errx(1, "Invalid res '%c'", res);
+	return p1;		/* DRAW */
 }
 
 int
@@ -75,21 +37,19 @@ main(int argc, char **argv)
 {
 	char buf[BSIZ];
 	FILE *fp = stdin;
-	enum shape p1;		/* Player 1 shape */
-	enum score_res res;
-	int score = 0;
+	int res = 0, p1, out;
 
 	if (argc > 1 && !(fp = fopen(argv[1], "rb"))) {
+		err(1, NULL);
 	}
 	while (fgets(buf, BSIZ, fp)) {
-		p1 = parse(buf[0]);
-		res = score_res(buf[2]);
-		score += res;
-		score += score_res_shape(res, p1);
+		p1 = parse_shape(buf[0]);
+		out = parse_out(buf[2]);
+		res += score[out] + score[out2shape(out, p1)];
 	}
 	if (fp != stdin && fclose(fp) == EOF) {
 		err(1, NULL);
 	}
-	printf("%d\n", score);
+	printf("%d\n", res);
 	return 0;
 }
