05903284916b8f1475d762390e68d019ce61bcda
Ireneusz Gabryś <ireneusz.gabrys@optioincentives.no>
2022-12-02 08:40:04 +0100

    Refactor 02a

--- 02a.c
+++ 02a.c
@@ -3,62 +3,31 @@
 
 #define BSIZ    8		/* Buffer size */
 
-enum shape { ROCK, PAPER, SCISSORS };
-enum score_shape {
-	SCORE_ROCK     = 1,
-	SCORE_PAPER    = 2,
-	SCORE_SCISSORS = 3
-};
-enum score_res {
-	SCORE_LOST     = 0,
-	SCORE_DRAW     = 3,
-	SCORE_WON      = 6
-};
+enum { ROCK = 0, PAPER, SCISSORS, LOST, DRAW, WON };
+int score[] = { 1, 2, 3, 0, 3, 6 };
 
-enum shape
+int
 parse(char c)
 {
 	switch (c) {
-	case 'A':
-	case 'X':
-		return ROCK;
-	case 'B':
-	case 'Y':
-		return PAPER;
-	case 'C':
-	case 'Z':
-		return SCISSORS;
+	case 'A': case 'X': return ROCK;
+	case 'B': case 'Y': return PAPER;
+	case 'C': case 'Z': return SCISSORS;
 	}
 	errx(1, "Invalid shape '%c'", c);
 }
 
-enum score_shape
-score_shape(enum shape s)
+int
+outcom(int p1, int p2)
 {
-	switch (s) {
-	case ROCK:     return SCORE_ROCK;
-	case PAPER:    return SCORE_PAPER;
-	case SCISSORS: return SCORE_SCISSORS;
-	}
-	errx(1, "Invalid shape '%c'", s);
-}
+	if (p1 == p2) return DRAW;
 
-enum score_res
-score_res(enum shape p1, enum shape p2)
-{
-	if (p1 == p2) {
-		return SCORE_DRAW;
-	}
-	if (p1 == ROCK) {
-		return p2 == PAPER ? SCORE_WON : SCORE_LOST;
-	}
-	if (p1 == PAPER) {
-		return p2 == SCISSORS ? SCORE_WON : SCORE_LOST;
-	}
-	if (p1 == SCISSORS) {
-		return p2 == ROCK ? SCORE_WON : SCORE_LOST;
-	}
-	errx(1, "Unhandled case of %d vs %d", p1, p2);
+	if ((p1 == ROCK     && p2 == PAPER)    ||
+	    (p1 == PAPER    && p2 == SCISSORS) ||
+	    (p1 == SCISSORS && p2 == ROCK))
+		return WON;
+
+	return LOST;
 }
 
 int
@@ -66,20 +35,19 @@ main(int argc, char **argv)
 {
 	char buf[BSIZ];
 	FILE *fp = stdin;
-	enum shape p1, p2;	/* Player 1 and 2 shape */
-	int score = 0;
+	int p1, p2, res = 0; /* Player 1, 2 shape, and final result */
 
 	if (argc > 1 && !(fp = fopen(argv[1], "rb"))) {
 	}
 	while (fgets(buf, BSIZ, fp)) {
 		p1 = parse(buf[0]);
 		p2 = parse(buf[2]);
-		score += score_shape(p2);
-		score += score_res(p1, p2);
+		res += score[p2];
+		res += score[outcom(p1, p2)];
 	}
 	if (fp != stdin && fclose(fp) == EOF) {
 		err(1, NULL);
 	}
-	printf("%d\n", score);
+	printf("%d\n", res);
 	return 0;
 }
