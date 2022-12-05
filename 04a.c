6dc8ad6749784e1a755b111a95cfacca09f151d0
irek <irek@gabr.pl>
2022-12-05 01:14:11 +0100

    04

---
+++ 04a.c
@@ -0,0 +1,16 @@
+#include "online.h"
+
+int res=0, a0, a1,  b0, b1;
+
+void online(char *line)
+{
+	sscanf(line, "%d-%d,%d-%d", &a0, &a1, &b0, &b1);
+	if ((a0 >= b0 && a1 <= b1) ||
+	    (b0 >= a0 && b1 <= a1)) {
+		res++;
+	}
+}
+void onend()
+{
+	printf("%d\n", res);
+}
