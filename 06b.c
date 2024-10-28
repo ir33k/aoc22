3b2e412626f3e7fc80713e346099efe612a968ed
irek <ireneusz.gabrys@optioincentives.no>
2024-10-28 13:20:29 +0100

    Try to improve 06b

--- 06b.c
+++ 06b.c
@@ -1,5 +1,22 @@
-/* Same code as in first part except the value of SIZ macro. */
+/* Same code as in first part except the value of SIZ macro.
+
+Playing around after watching "This Algorithm is 1,606,240% FASTER" [1] by ThePrimeagen.
+
+[1] https://www.youtube.com/watch?v=U16RnpV48KQ
+
+Initial speed tests on MacOS of current solution:
+
+	$ cc -Wall -Wextra -Wshadow -Wswitch-enum -pedantic -std=c89 06b.c -o 06b && time ./06b <06.in5
+	3986
+	
+	real	0m0.161s
+	user	0m0.001s
+	sys	0m0.001s
+
+*/
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #define SIZ	14		/* Number values to compare */
 
@@ -12,21 +29,21 @@ diff(char *list, int siz)
 	for (i = 0;   i < siz-1; i++)
 	for (j = i+1; j < siz;   j++) {
 		if (!list[j] || list[i] == list[j]) {
-			return 0; /* Not different */
+			return 0;	/* Not different */
 		}
 	}
-	return 1;		/* All different */
+	return 1;	/* All different */
 }
+
 
 int
 main(void)
 {
-	int i;
+	unsigned i=0;
 	char c, last[SIZ]={0};
-
 	while ((c = getchar()) != EOF && !diff(last, SIZ)) {
 		last[i++ % SIZ] = c;
 	}
-	printf("%d\n", i);
+	printf("%u\n", i);
 	return 0;
 }
